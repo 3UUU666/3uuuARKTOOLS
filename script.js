4f6da79aa858f056461a297e39313e4b28c11f8f
3UUU666 <gg111444777gg@gmail.com>
2025-02-04 15:33:35 +0800

    D表格新增

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ new Vue({
       { name: '石頭', blue: 10, green: 10, white: 10 },
       { name: '魚(綠珍珠)', blue: 10, green: 10, white: 10 },
       { name: '狩獵(綠皮革)', blue: 10, green: 10, white: 10 }
-    ]
+    ],
+    productionCost: 0  // 用於保存 D 表格的輸入數字
   },
   computed: {
     computedItems() {
